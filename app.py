2eb4d7b0c492f7a0f829e8736c2094fe31abaf39
usamaSherali <60126775+usamaSherali@users.noreply.github.com>
2025-07-12 16:16:22 +0500

    app.py

--- app.py
+++ app.py
@@ -17,4 +17,6 @@ def download_video():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
